141a6b07295952012cafdfe83b617a2611f1d7cf
SidgarPraveena <37445157+SidgarPraveena@users.noreply.github.com>
2020-06-05 08:51:23 +0530

    Nth smallest number using quicksort

---
+++ Assignment-4/2019201042_Q1.cpp
@@ -0,0 +1,69 @@
+#include <cstdlib> 
+#include <iostream> 
+using namespace std; 
+
+int partition(int arr[], int low, int high) 
+{ 
+	int pivot = arr[high]; 
+	int i = (low - 1); 
+
+	for (int j = low; j <= high - 1; j++) 
+	{ 
+		if (arr[j] <= pivot) 
+		{ 
+			i++;
+			swap(arr[i], arr[j]); 
+		} 
+	} 
+	swap(arr[i + 1], arr[high]); 
+	return (i + 1); 
+} 
+int partition_r(int arr[], int low, int high) 
+{ 
+	srand(time(NULL)); 
+	int random = low + rand() % (high - low); 
+	swap(arr[random], arr[high]); 
+	return partition(arr, low, high); 
+} 
+void quickSort(int arr[], int low, int high,int pos,int n,int &temp) 
+{ 
+	if (low < high) 
+	{
+		int pi = partition_r(arr, low, high);
+		if(pi == pos)
+		{
+			temp=1;
+			cout<<arr[pi]<<endl;
+		}
+		else if(pi < pos)
+		{
+			quickSort(arr, pi + 1, high,pos,n,temp);
+		}
+		else
+		{
+			quickSort(arr, low, pi - 1,pos,n,temp); 
+		}
+	} 
+} 
+
+int main() 
+{ 
+	
+	int n,temp=0;
+	cout<<"Enter n:";
+	cin>>n;
+	int arr[n];
+	for(int i=0;i<n;i++)
+	{
+		cout<<"Enter "<<i+1<<" ele ";
+		cin>>arr[i];
+	}
+	int pos;
+	cout<<"Enter k: ";
+	cin>>pos;
+	quickSort(arr, 0, n-1 ,pos,n,temp);
+	if(temp==0)
+		cout<<arr[pos]<<endl; 
+	return 0; 
+} 
+
